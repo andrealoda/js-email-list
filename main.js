6a39aacb9f30824b07a8494a7ab973b969401998
andrealoda <andrea@casaloda.it>
2026-04-01 19:01:30 +0200

    initial commit and preliminary  considerations

---
+++ main.js
@@ -0,0 +1,17 @@
+// Attraverso l'apposita API di Boolean
+// https://flynn.boolean.careers/exercises/api/random/mail
+// generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+
+// Bonus
+
+// Abbellire con CSS o Bootstrap
+// Inserire un bottone che al click faccia il fetch altre 10 mail (sostituendo le altre)
+
+
+/*
+
+creare array vuota
+ciclo for con i <10 i++
+nel ciclo, ogni i pusha nell'array ciò che restituisce l'API
+
+in pagina inserisco ogni elemento dell'array in una riga della lista.
